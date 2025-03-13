1467c0d40f7a0ea9de0d6d0c1673ff10e253640f
ABENEZER TESSEMA <abenezer4989@gmail.com>
2025-03-13 08:26:58 -0500

    Some analytics changes

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import { Container } from './LayoutStyles'
+import {Analytics} from "@vercel/analytics/react";
 
 export const Layout = ({children}) => {
   return (
