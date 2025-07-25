527ce6813596dda07b9016b6270b0399c00c8f34
ABENEZER TESSEMA <abenezer4989@gmail.com>
2025-07-24 22:47:38 -0500

    Add milestone for starting StayOnTrack project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -41,6 +41,7 @@ export const TimeLineData = [
   {month:months[1-1], year: 2024, text: 'Built My First Web app'},
   {month:months[4-1], year: 2024, text: 'Built My First Full Stack Application'},
   {month:months[5-1], year:2024, text:'Software Development Intern at PolySense Solutions'},
+  {month:months[6-1], year:2024, text:'Started building StayOnTrack'},
   {month:months[9-1], year: 2024, text: 'Began Evolving in my skills as a developer and a mentor.' },
   {month:months[10-1], year: 2024, text: 'Involved in the development of an open source PWA for a non-profit.' },
   {month:months[4-1], year: 2025, text: 'Implemented a Secure API Gateway using Spring Boot, Redis, KeyCloak.' }
